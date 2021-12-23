4011b811e6effc33a1eb38a818d86ccd1551e1ad
dinesh-kumar-c <deena_dinesh@yahoo.co.in>
2021-12-23 19:01:22 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -4,14 +4,14 @@
 if [ "$1" = "-f" ]
 then
 	 echo "logging into Centos Dev VM.... "
-	 ssh dkumar@x.x.x.x
+	 ssh user@x.x.x.x
 else
 	echo 'do you want to login to TT Centos Dev vm? (Y/N) : '
 	read userinp
 	case $userinp in
 		[yY][eE][sS]|[yY])
 	 echo "logging into Centos Dev VM.... "
-	 ssh dkumar@x.x.x.x
+	 ssh user@x.x.x.x
 	 ;;
 		[nN][oO]|[nN])
 	 echo "Welcome! This is Ubuntu 20.04 laptop"
